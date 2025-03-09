97694c613f84a8bc0f4e0349ecbd083c80a22c92
Sofia <sofia.sarasty.12@gmail.com>
2025-03-09 13:10:39 -0500

    Refactor StartMenu component structure and styles for improved layout and background handling

--- src/screens/StartMenu/StartMenu.tsx
+++ src/screens/StartMenu/StartMenu.tsx
@@ -4,17 +4,17 @@ interface startmenutypes {
     Onclick: () => void
 }
 
-function StartMenu({ Onclick }: startmenutypes){
-
-
-    return(
-        <>
-        <div className='screeninicio'></div>
-        <p>El juego para terminarlo ¡Lo más pronto posible!</p>
-        <button onClick={Onclick}>Tap to start</button>
-        </>
-    )
+function StartMenu({ Onclick }: startmenutypes) {
+    return (
+        <div className='startmenu-body'>
+            <div className='screeninicio'>
+                <p>El juego para terminarlo ¡Lo más pronto posible!</p>
+                <button onClick={Onclick}>Tap to start</button>
+            </div>
+        </div>
+    );
 }
-export default StartMenu
+
+export default StartMenu;
 
 
