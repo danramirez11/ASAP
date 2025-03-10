12b97feae2cc265f09a397118e4119017b523d6e
viegosabroso <Thommmmmmas@hotmail.com>
2025-03-09 21:03:45 -0500

    Lol que mal lo siento

--- src/screens/Intermision/Intermision.tsx
+++ src/screens/Intermision/Intermision.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import { Rounds, Status } from "../../types/types"
+import './Intermision.css'
 
 const Intermision = ({rounds, status, Onclick} : {rounds: Rounds; status: Status; Onclick: () => void}) => {
     const handle = () => {
@@ -17,11 +18,15 @@ const Intermision = ({rounds, status, Onclick} : {rounds: Rounds; status: Status
 
     return (
         <>
+        <div className="intermisionBackground">
+
             <h1>{rounds[status.round].games[status.game].type === 'comp' ? 'Es hora de un VS' : '¡A trabajar en equipo!'}</h1>
             <p>¿Quien se lleva la gloria?</p>
+            <div className="versus">
             <p className="vs-player">{rounds[status.round].games[status.game].players[0]}</p>
-            <h3>vs</h3>
             <p className="vs-player">{rounds[status.round].games[status.game].players[1]}</p>
+            </div>
+        </div>
         </>
     )
     }
