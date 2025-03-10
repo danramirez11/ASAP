cd3c36d8e8e10eb73eb7b3f9777731ac45c0a972
viegosabroso <Thommmmmmas@hotmail.com>
2025-03-09 23:08:40 -0500

    styles finished

--- src/screens/Card/Card.tsx
+++ src/screens/Card/Card.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Rounds, Status, Teams } from "../../types/types";
 import "./selectwinner.css";
-
+import "./Card.css"
 type CardType = {
   teams: Teams;
   rounds: Rounds;
@@ -11,13 +11,38 @@ type CardType = {
 }
 
 const Card = ({ teams, rounds, status, selectWinner }: CardType) => {
+  const handleimages = (Category: string) => {
+    const backgroundimage = document.getElementById("card-body") as HTMLDivElement;
+    const images = {
+      "plastilina": "/src/assests/imgs/plastilina.png",
+      "mimica": "/src/assests/imgs/Mimica.png",
+      "dibujar": "/src/assests/imgs/Dibujar.png",
+      "dados": "/src/assests/imgs/dados.png",
+    }
+  
+  
+    if(Category === "Moldeo"){
+      backgroundimage.style.backgroundImage = `url(${images.plastilina})`;
+    } else if (Category === "Mímica") {
+      backgroundimage.style.backgroundImage = `url(${images.mimica})`;
+    } else if (Category === "Dibujo") {
+      backgroundimage.style.backgroundImage = `url(${images.dibujar})`;
+    } else if (Category === "Palabras") {
+      backgroundimage.style.backgroundImage = `url(${images.dados})`;
+    }
+  };
+
+  useEffect(() => {
+    handleimages(rounds[status.round].games[status.game].cathegory);
+    console.log(rounds[status.round].games[status.game].cathegory);
+  }, [status.game, status.round]);
 
   const [clicked, setClicked] = useState(false);
-  
+
   const handleWinnerSelection = (team: 'team1' | 'team2') => {
     console.log(`Winner: ${teams[team].name}`);
     const type = rounds[status.round].games[status.game].type;
-    
+
     if (type === 'coop') {
       selectWinner(type, team === 'team1' ? teams.team1.name : teams.team2.name);
     } else {
@@ -25,16 +50,15 @@ const Card = ({ teams, rounds, status, selectWinner }: CardType) => {
         selectWinner(type, rounds[status.round].games[status.game].players[0])
       } else {
         selectWinner(type, rounds[status.round].games[status.game].players[1]);
-      } 
+      }
     }
 
     setClicked(false);
+  };
 
-} ;
-
-  console.log(teams)
-  console.log(rounds)
-  console.log(status)
+  console.log(teams);
+  console.log(rounds);
+  console.log(status);
 
   console.log(rounds[status.round].games[status.game].players[0])
   if (rounds[status.round].games.length === 0) {
@@ -58,16 +82,17 @@ const Card = ({ teams, rounds, status, selectWinner }: CardType) => {
         </>
         :
         <>
-          <h1>{rounds[status.round].games[status.game].name}</h1>
-          <p>{rounds[status.round].games[status.game].players[0]} vs {rounds[status.round].games[status.game].players[1]}</p>
-          <p>{rounds[status.round].games[status.game].card}</p>
+        <div id="card-body" >
+          <h1 className="card-title"><span className="encounterplayers">{rounds[status.round].games[status.game].players[0]} vs {rounds[status.round].games[status.game].players[1]}</span> <span>{rounds[status.round].games[status.game].name}</span></h1>
+          <p className="challenge">{rounds[status.round].games[status.game].card}</p>
           { rounds[status.round].games[status.game].subCathegory !== 'none' &&
             <div className="card-cathegory">
-              <h2>Categoría</h2>
-              <h4>{rounds[status.round].games[status.game].subCathegory}</h4>
+              <h2 className="categorytitle">Categoría</h2>
+              <h4 className="categorycard">{rounds[status.round].games[status.game].subCathegory}</h4>
             </div>
           }
           <button onClick={() => setClicked(true)}>ASAP! ¿Quién fue el ganador?</button>
+          </div>
         </>
       }
     </div>
